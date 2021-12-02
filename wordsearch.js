b53a5f4cf7d0b34b61fa232560701d18078b563b
Mohamed Ali <12maali27@gmail.com>
2021-12-02 01:57:43 +0000

    finished functionality

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let verticalMatrix = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    verticalMatrix.push([]);
+  }
+  for (let x = 0; x < letters.length; x++) {
+    for (let j = 0; j < letters[j].length; j++) {
+      verticalMatrix[j][x] = letters[x][j];
     }
-}
+  }
+  const verticalJoin = verticalMatrix.map((ls) => ls.join(""));
+  for (let h of verticalJoin) {
+    if (h.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
